ce6c19f533fe2b83e3207a000eb45352dd6b3b55
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-04-09 13:52:12 +0300

    Add changes to different areas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'quotes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('quotes');
+    expect(app.quotes).toEqual('quotes');
   });
 
   it('should render title', () => {
